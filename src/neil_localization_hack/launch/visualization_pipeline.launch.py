8feb4e65dfac64e07bf0844af0500db68429da48
neilkhadilkar23 <neilkhadilkar23@berkeley.edu>
2023-08-10 15:30:17 -0700

    visualization incorporated into launch for use with zed

--- src/neil_localization_hack/launch/visualization_pipeline.launch.py
+++ src/neil_localization_hack/launch/visualization_pipeline.launch.py
@@ -1,5 +1,7 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+import os
 
 def generate_launch_description():
    return LaunchDescription([
@@ -14,5 +16,18 @@ def generate_launch_description():
             namespace='path_publisher',
             executable='path_publisher',
             name='path_publisher'
+        ),
+        Node(
+         package="tf2_ros",
+         executable="static_transform_publisher",
+         name="swri_transform",
+         arguments=["0", "0", "0", "0", "0", "0", "odom", "base_link"]
+        ),
+        Node(
+            package='rviz2',
+            namespace='',
+            executable='rviz2',
+            name='rviz2',
+            arguments=['-d', [os.path.join(get_package_share_directory("neil_localization_hack"), 'params', 'localization_with_zed.rviz')]]
         )
    ])
